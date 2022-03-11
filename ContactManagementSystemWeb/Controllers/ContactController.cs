0712a98e287b92a0c359c3c7ee23322e81c7c0f9
Manish Kumar <manishkumar.ece@gmail.com>
2022-03-10 17:28:40 -0800

    Added the Web MVC project to solution. Removed default controllers and views (cleanup)

---
+++ ContactManagementSystemWeb/Controllers/ContactController.cs
@@ -0,0 +1,94 @@
+﻿using ContactManagementSystemModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagementSystemWeb.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IConfiguration Configuration;
+
+        public ContactController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // GET: ContactController
+        HttpClient client;
+        public ActionResult Index()
+        {
+            List<Contact> contacts = new List<Contact>();
+            client.BaseAddress = new Uri(Configuration["BaseUrl"]);
+            return View();
+        }
+
+        // GET: ContactController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ContactController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContactController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ContactController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContactController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ContactController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
