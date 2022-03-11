8663f5a5896221c060c6d8ae34b86c3ea87ae1ea
Manish Kumar <manishkumar.ece@gmail.com>
2022-03-11 00:10:14 -0800

    Add unit test to test index function in ContactsController

---
+++ ContactManagementSystemWebTests/ContactControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using ContactManagementSystemWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContactManagementSystemWebTests
+{
+    public class ContactControllerTests
+    {
+        [Fact]
+        public async Task IndexTest()
+        {
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.SetupGet(x => x[It.Is<string>(s => s == "BaseUrl")]).Returns("https://localhost:7100/api/");
+            var controller = new ContactController(mockConfig.Object);
+
+            var result = await controller.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+        }
+    }
+}
