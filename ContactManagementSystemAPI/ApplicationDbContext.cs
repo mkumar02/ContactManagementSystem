1cf272ba8f1e1860ee671267a8c01aa4df08eb72
Manish Kumar <manishkumar.ece@gmail.com>
2022-03-10 16:11:13 -0800

    Added connectionstring and db context class. Updated Program file to use dbcontext

---
+++ ContactManagementSystemAPI/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactManagementSystemModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagementSystemAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
